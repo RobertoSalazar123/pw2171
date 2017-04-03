f2d252d624fd3bcba902914e36e10498bc1a2173
CCDM 26 <darkrober@outlook.com>
2017-04-03 15:03:49 -0600

    Ejemplo JavaScript

---
+++ js/sumas.js
@@ -0,0 +1,16 @@
+function texto{
+	alert(document.getElementById("texto1").value)
+}
+function suma(a,b){
+	return a+b
+}
+var suma2 = function(a,b){
+	return a+b
+}
+function texto{
+	alert(document.getElementById("texto2").value)
+}
+function suma2(){
+var n1 =document.getElementById("num1").value
+var n2 =document.getElementById("num2").value
+}
