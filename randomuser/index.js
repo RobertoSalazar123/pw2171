b057340bc647affc89554280279fde8b88067f27
CCDM 26 <darkrober@outlook.com>
2017-04-26 14:59:50 -0600

    cambios usuarioRandom

--- randomuser/index.js
+++ randomuser/index.js
@@ -8,6 +8,12 @@ $.ajax({
   success: function(data) {
   	$("#txtNombre").html(data.results[0].name.first+" "+data.results[0].name.last)
   	$("#imgFoto").attr("src",data.results[0].picture.large)
+  	$("#txtTitle").html(data.results[0].name.title)
+  	$("#txtGenero").html(data.results[0].name.gender)
+  	$("#txtCalle").html(data.results[0].location.street)
+  	$("#txtCiudad").html(data.results[0].location.city)
+  	$("#txtEstado").html(data.results[0].location.state)
+  	$("#txtCodigoP").html(data.results[0].location.postcode)
   },
   error(a,b,c){
   	alert("sin internet o sin servidor");
